f5b1726159c4b0cf8cdd856d42e9195961b3bba4
AhmerAli1813 <98941104+AhmerAli1813@users.noreply.github.com>
2022-04-18 02:49:00 -0700

    start

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// let navbar = document.getElementById('mynavbar');
+// let show =document.getElementById("show").addEventListener('click',()=>{
+// alert("hi")
+// });
+
+
+// let menu = document.querySelector('#menu-bar');
+// let navbar = document.querySelector('#mynavbar');
+
+// menu.onclick = () =>{
+// alert("df")
+// }
+
+
+// let menu = document.getElementById("menu-bar");
+// let navbar = document.getElementById("mynavbar");
+
+// menu.onclick = () =>{
+// alert("df")
+// }
+
+// navbar.style.display='block';
