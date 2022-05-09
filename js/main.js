f2b99862c1325d5256939835644489d4bd4705f5
AhmerAli1813 <98941104+AhmerAli1813@users.noreply.github.com>
2022-05-09 07:08:46 -0700

    complete navbar

--- js/main.js
+++ js/main.js
@@ -19,13 +19,18 @@ window.addEventListener("scroll" ,function(){
     var downhead = document.querySelector('.down-head');
     downhead.classList.toggle("stocky" , window.scrollY > 0);
     
-    var button = document.querySelector('.mycolor');
-    button.classList.toggle('btn-outline-dark' ); 
-    button.classList.remove('btn-outline-secondary' );   
-
+    
     var navbaritems = document.querySelector('.navbar');
+    if(window.scrollY==0){
+        navbaritems.classList.remove('navbar-light');
+    navbaritems.classList.add('navbar-dark' );
+    }
+    else{
+        
     navbaritems.classList.remove('navbar-dark');
-    navbaritems.classList.add('navbar-light' );
+        navbaritems.classList.add('navbar-light' );
+    }
+    
     
     
 });
